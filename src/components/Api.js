0eb4452abdb9dd21cc0d978ac99c2ea9afa23ccd
lmoron94 <lucasmoron0512@gmail.com>
2023-04-01 01:42:32 -0300

    Se importa bootstrap. Se permite consultar turnos previamente guardados en la base de datos.

--- src/components/Api.js
+++ src/components/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const API_URL = 'http://localhost:8081';
 
 export const turnoById = (id) => {
-	return axios.get(`${API_URL}/turno/`)
+	return axios.get(`${API_URL}/turnos/${id}`)
 	.then(response => {
 		console.log(response);
 		return response.data
--- src/components/Turno.js
+++ src/components/Turno.js
@@ -1,40 +1,76 @@
 import React, { useState, useEffect } from 'react';
 import { turnoById } from './Api.js';
+import { useParams } from 'react-router-dom';
 
 const Turno = (props) => {
     const [turnoData, setTurnoData] = useState({
-    	mensaje: "Sin mensaje",
+    	fechaInicio: "",
+        horaInicio: "",
+        horaFin: "",
+        tipo: "Sin asignar",
+        pacienteId: null
+
     });
 
-    let id = 1
-/*
+    let id = 1;
+
     useEffect(() => {
         getTurnoData();
     }, [])
-*/
+
 
     const getTurnoData = () => {
         turnoById(id)
             .then(
             data => {
                 setTurnoData({...turnoData,
-                    mensaje: data.mensaje,
+                    fecha: formatFecha(data.horarioInicio),
+                    horaInicio: formatHora(data.horarioInicio),
+                    horaFin: formatHora(data.horarioFin),
+                    tipo: data.tipo,
+                    pacienteId: data.pacienteId,
+
                 })       
             })
             .catch(error => {
                 setTurnoData({
                     ...turnoData,
-                    mensaje: "ERROR",
+                    tipo: "ERROR",
                 })
             })
     }
 
+    const formatFecha = (fechaArray) => {
+        let fechaFormateada = fechaArray[0] + "-" + fechaArray[1] + "-" + fechaArray[2]
+        return fechaFormateada
+    }
+
+    const formatHora = (horaArray) => {
+        let horaFormateada = horaArray[3] + ":" + horaArray[4]
+        return horaFormateada
+    }
 
     return (
     	<div className="container">
-    		<h1>Test</h1>
-    		<h3> {turnoData.mensaje} </h3>
-    			<button type="button" class="btn btn-primary" onClick={getTurnoData}> Enviar Request	 </button>
+    		<h1>Turno</h1>
+    		<h3> {turnoData.tipo} </h3>
+            <h4> Paciente: { turnoData.pacienteId } </h4>
+
+            <div class="row">
+                <h5 class="col-md-2 control-label">Fecha turno:</h5>
+                <div class="col-md-2">
+                    <h5 id="fecha"> {turnoData.fecha} </h5>
+                </div>
+                <h5 for="horaInicio" class="col-md-2 control-label">Hora inicio:</h5>
+                <div class="col-md-2">
+                    <h5 id="horaInicio"> {turnoData.horaInicio} </h5>
+                </div>
+                <h5 for="horaFin" class="col-md-2 control-label">Hora fin:</h5>
+                <div class="col-md-2">
+                    <h5 id="horaFin"> {turnoData.horaFin} </h5>
+                </div>
+            </div>
+            
     	</div>
     )
 
