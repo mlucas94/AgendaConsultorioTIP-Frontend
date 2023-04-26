bf512ae9ab34aab08cae70204386e1c7ae292a99
lmoron94 <lucasmoron0512@gmail.com>
2023-04-26 00:21:12 -0300

    Test disenio sidebar

--- src/components/BarraNavegacion.js
+++ src/components/BarraNavegacion.js
@@ -3,34 +3,45 @@ import {Nav, Row, Col} from 'react-bootstrap';
 import "./css/BarraNavegacion.css"
 
 
+
 const BarraNavegacion = (props) => {
     
+    const mostrarTitulo = () => {
+        var titulo = window.location.pathname;
+        titulo = titulo.replace("_", " ").replace("/", "")
+        titulo = titulo.charAt(0).toUpperCase() + titulo.slice(1)
+        console.log(titulo)
+        return titulo ? titulo : "Inicio"
+    }
+
     return(
 
-        <Container fluid>
-
-
-
+        <div>
 
         <Nav
         activeKey="/home"
-        onSelect={selectedKey => alert(`selected ${selectedKey}`)}
         >
-        <div className='d-flex flex-column'>
+        <div className='d-flex flex-column sidebar'>
 
-        <div className='p-2'>
-            <Nav.Link href="/">Inicio</Nav.Link>
+            <div className='sidebar-title'>
+                <h4>{mostrarTitulo()}</h4>
+            </div>
+
+        <div className='pt-2'>
+            <Nav.Link href="/" className="bg-secondary text-white">
+                <div className="ps-2">Inicio</div>
+            </Nav.Link>
         </div>
 
         <div className='p-2 '>
-            <Nav.Link eventKey="link-1">Turnos</Nav.Link>
+            <Nav.Link href="/calendario">Turnos</Nav.Link>
             </div>
             <div className='p-2'>
-            <Nav.Link eventKey="link-2">Pacientes</Nav.Link>
+            <Nav.Link href="/">Pacientes</Nav.Link>
             </div>
             </div>
         </Nav>
-        </Container>
+        </div>
         
     )
 
